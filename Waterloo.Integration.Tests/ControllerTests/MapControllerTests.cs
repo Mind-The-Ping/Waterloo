993f979c86546365a10c39f5f424bc2f7b6b518a
Kinaetron <darren.daley@hotmail.co.uk>
2025-11-20 11:13:15 +0000

    donzo

--- Waterloo.Integration.Tests/ControllerTests/MapControllerTests.cs
+++ Waterloo.Integration.Tests/ControllerTests/MapControllerTests.cs
@@ -30,7 +30,7 @@ public class MapControllerTests : IClassFixture<CustomWebApplicationFactory>
         response.EnsureSuccessStatusCode();
 
         var result = await response.Content.ReadFromJsonAsync<IEnumerable<Model.Line>>();
-        result.Count().Should().Be(15);
+        result.Count().Should().Be(16);
     }
 
     [Fact]
