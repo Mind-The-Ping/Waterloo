2f89725281edae9d0002c364eac0dadb1262d86b
Kinaetron <darren.daley@hotmail.co.uk>
2025-11-22 14:27:38 +0000

    mostly done

--- Waterloo.Repository.Unit.Tests/LineUnitTests.cs
+++ Waterloo.Repository.Unit.Tests/LineUnitTests.cs
@@ -13,7 +13,7 @@ public class LineUnitTests
         var result = _repository.GetAll();
 
         result.Should().NotBeNullOrEmpty();
-        result.Count().Should().Be(19);
+        result.Count().Should().Be(20);
     }
 
     [Fact]
