67aa269134007c5efbabfed7fad56c18e5116066
Kinaetron <darren.daley@hotmail.co.uk>
2025-12-04 10:52:17 +0000

    weezo

---
+++ Waterloo.Repository/GuidHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Waterloo.Repository;
+
+public static class GuidHelper
+{
+    public static Guid GuidFromString(string input)
+    {
+        input ??= string.Empty;
+        var normalized = input.Trim().ToLowerInvariant();
+        var bytes = Encoding.UTF8.GetBytes(normalized);
+        var hash = MD5.HashData(bytes);
+
+        return new Guid(hash);
+    }
+}
